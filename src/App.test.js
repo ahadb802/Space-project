54ce000377812a5a1dcea1c3c1bc9e16bad31e28
Oscar-Espinoza <oscardavid2094@gmail.com>
2023-01-30 17:40:16 -0300

    empty nav

--- src/App.test.js
+++ src/App.test.js
@@ -1,3 +1,5 @@
+/* eslint linebreak-style: ["error", "windows"] */
+
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {
+  BrowserRouter as Router,
+  Route,
+  NavLink
+} from "react-router-dom";
+
+export default function Navbar() {
+  return (
+    <nav>Nav</nav>
+  )
+}
