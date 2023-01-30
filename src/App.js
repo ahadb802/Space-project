fa96c3e1b2315a2ab407774d3617987686725e06
Oscar-Espinoza <oscardavid2094@gmail.com>
2023-01-30 18:25:41 -0300

    navbar first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
+import {
+  BrowserRouter,
+  Route,
+  Routes,
+} from 'react-router-dom';
 import Navbar from './Components/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" />
+          <Route path="/missions" />
+          <Route path="/profile" />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
