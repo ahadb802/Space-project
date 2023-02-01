fff62522f3f3b6f9afa5494fe6df0f7303f5f04a
Oscar-Espinoza <oscardavid2094@gmail.com>
2023-02-01 10:08:37 -0300

    missions render ready

--- src/Components/pages/Mission/Mission.js
+++ src/Components/pages/Mission/Mission.js
@@ -23,12 +23,20 @@ export default function Mission() {
           </tr>
         </thead>
         <tbody>
-          {missions.map((mission) => (
-            <tr key={mission.mission_name}>
-              <td>{mission.mission_name}</td>
-              <td>{mission.description}</td>
-              <td>Member</td>
-              <td>Leave mission</td>
+          {missions.map((mission, i) => (
+            <tr key={mission.mission_name} className={i % 2 === 0 ? 'pair' : 'odd'}>
+              <td className="mission-name">{mission.mission_name}</td>
+              <td className="description">{mission.description}</td>
+              <td>
+                <p className={`${i % 2 === 0 ? 'active' : 'inactive'} member`}>
+                  {i % 2 === 0 ? 'Active member' : 'NOT A MEMBER'}
+                </p>
+              </td>
+              <td>
+                <div aria-label="join or leave mission" className={`${i % 2 === 0 ? 'leave' : 'join'} button`}>
+                  {i % 2 === 0 ? 'Leave Mission' : 'Join Mission'}
+                </div>
+              </td>
             </tr>
           ))}
         </tbody>
