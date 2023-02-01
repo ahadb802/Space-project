5342d41632d4a535dbfa77c5a72a8d4d6630c94c
ahadb802 <abdulahadb802@gmail.com>
2023-02-01 05:37:33 +0500

    added functionality to reserved keyword

--- src/Components/singlerocket/SingleRocket.js
+++ src/Components/singlerocket/SingleRocket.js
@@ -1,28 +1,43 @@
 import React from 'react';
 import propTypes from 'prop-types';
 import './SingleRocket.css';
+import { useDispatch } from 'react-redux';
+import { reservedRocket } from '../redux/Rocket-state/Rocket';
 
 const SingleRocket = ({
-  id, name, description, image,
-}) => (
-  <div className="wraper" key={id}>
-    <div className="rocket-container">
-      <img src={image} alt="rocket" />
-      <div className="rocket-info">
-        <h3 className="title">{name}</h3>
-        <p className="rocket-description">{description}</p>
-        <button className="btn" type="button">
-          Reserve Rocket
-        </button>
+  id, name, description, image, reserved,
+}) => {
+  const check = useDispatch();
+  const checkReservation = () => {
+    check(reservedRocket(id));
+  };
+  return (
+    <div className="wraper" key={id}>
+      <div className="rocket-container">
+        <img src={image} alt="rocket" />
+        <div className="rocket-info">
+          <h3 className="title">{name}</h3>
+          <p className="rocket-description">
+            {reserved ? (
+              <span className="reserved">Reserved</span>
+            ) : (
+              ''
+            )}
+            {description}
+          </p>
+          <button className="btn" type="button" onClick={checkReservation}>
+            {reserved ? 'Cancel Reservation' : 'Reserve Rocket'}
+          </button>
+        </div>
       </div>
     </div>
-  </div>
-);
-
+  );
+};
 SingleRocket.propTypes = {
   id: propTypes.string.isRequired,
   name: propTypes.string.isRequired,
   description: propTypes.string.isRequired,
   image: propTypes.string.isRequired,
+  reserved: propTypes.bool.isRequired,
 };
 export default SingleRocket;
